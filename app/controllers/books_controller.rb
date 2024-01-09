50c1635c758845121a4b17f5309160eff51b1cc3
miyoooon <133766145+miyoooon@users.noreply.github.com>
2024-01-09 13:23:12 +0000

    [Finish] Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,32 +2,47 @@ class BooksController < ApplicationController
 
   def index
     @newbook = Book.new
-    # @book = Book.find(params[:id])
     @books = Book.all
   end
 
   def create
     @newbook = Book.new(book_params)
     if @newbook.save
-      flash[:notice] = "Book was successfully created."
+      flash[:notice] = "投稿に成功しました。"
       redirect_to book_path(@newbook.id)
     else
+      flash.now[:notice] = "投稿に失敗しました。"
       @books = Book.all
       render :index
     end
   end
 
   def show
-
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "更新しました。"
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:notice] = "更新に失敗しました。"
+      render :edit
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      redirect_to books_path
+    else
+      render :edit
+    end
   end
 
   private
--- app/controllers/homes/top_controller.rb
+++
@@ -1,2 +0,0 @@
-class Homes::TopController < ApplicationController
-end
